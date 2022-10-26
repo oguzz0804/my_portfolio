2d8eeffdbc8ec485777b940ab4569ff2e147d6f4
Oğuzhan Ötleş <oguzz0804@gmail.com>
2022-10-26 22:56:45 +0300

    First commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/oguzz0804/" className="home__social-icon">
+                <i class="uil uil-instagram-alt"></i>
+            </a>
+            <a href="https://github.com/oguzz0804" className="home__social-icon">
+                <i class="uil uil-github"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
